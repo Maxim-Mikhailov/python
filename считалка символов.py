c32a0f0a2ef35cb65ecaa68b1834655c6cab6303
Maxim Mikhailov <101477410+Maxim-Mikhailov@users.noreply.github.com>
2022-03-20 19:11:53 +0300

    считалка символов
    выводит кол-во символов в введенном слове

---
+++ считалка символов.py
@@ -0,0 +1,19 @@
+
+print("привет, просто введи свое имя или слово и  программа посчитает сколько в нем букв:)")
+
+name = input()
+def Hello(name):
+    print('привет, ' + name + '. Твое  имя/слово состоит из ' + str(len(name)) + ' букв.')
+spam = name
+Hello(name)
+name2 = input()
+def Hello1(name2):
+    print('привет, ' + name2 + '. Твое имя/слово состоит из ' + str(len(name2)) + ' букв.')
+spam1 = name2
+Hello(name2)
+name3 = input()
+def Hello2(name3):
+    print('привет, ' + name3 + '. Твое имя/слово состоит из ' + str(len(name3)) + ' букв.')
+
+spam2 = name3
+Hello(name3)
