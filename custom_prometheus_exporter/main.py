92758c96f33fc321ab3dcdc870c474cfd05ef426
Mikhailov Maksim <m.m.o.99@mail.ru>
2023-02-11 14:33:27 +0300

    test

--- custom_prometheus_exporter/main.py
+++ custom_prometheus_exporter/main.py
@@ -1,19 +1,31 @@
-from prometheus_client import start_http_server, Summary
-import random
+from prometheus_client import Gauge, Info, Summary, Histogram, Counter, start_http_server
+from http.server import HTTPServer, CGIHTTPRequestHandler
+import requests
 import time
 
-# Создание метрики для остлеживания затраченного времени и отслеживания запросов
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+#server_address = ("", 8000)
+#httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+#httpd.serve_forever()
 
-# декорация функции метрикой
-@REQUEST_TIME.time()
-def process_request(t):
-    """функция обманка которая требует немного времени"""
+response = requests.get('http://localhost:8001/mock/free_ip.txt')
+
+ip = Info('free_ip:', 'use_ip:')
+ip.info({response.text: 'use_ip'})
+
+# Create a metric to track time spent and requests made.
+#REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Decorate function with metric.
+
+def ip(t):
+    """A dummy function that takes some time."""
     time.sleep(t)
 
 if __name__ == '__main__':
-    # старт сервера
+    # Start up the server to expose the metrics.
     start_http_server(8000)
-    # генерация запросов
+    # Generate some requests.
     while True:
-        process_request(random.random())
+        print(response.text)
+
+
