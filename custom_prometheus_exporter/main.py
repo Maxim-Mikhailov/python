196e695e4a84175fcf0ae490b370a2cab54ae188
Mikhailov Maksim <m.m.o.99@mail.ru>
2023-02-12 18:41:21 +0300

    test

--- custom_prometheus_exporter/main.py
+++ custom_prometheus_exporter/main.py
@@ -1,24 +1,14 @@
 from prometheus_client import Gauge, Info, Summary, Histogram, Counter, start_http_server
-from http.server import HTTPServer, CGIHTTPRequestHandler
 import requests
 import time
-
-#server_address = ("", 8000)
-#httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-#httpd.serve_forever()
 
 response = requests.get('http://localhost:8001/mock/free_ip.txt')
 
 ip = Info('free_ip:', 'use_ip:')
-ip.info({response.text: 'use_ip'})
-
-# Create a metric to track time spent and requests made.
-#REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
-# Decorate function with metric.
+ip.info({response.text: ''})
 
 def ip(t):
-    """A dummy function that takes some time."""
+#    """A dummy function that takes some time."""
     time.sleep(t)
 
 if __name__ == '__main__':
@@ -27,5 +17,10 @@ if __name__ == '__main__':
     # Generate some requests.
     while True:
         print(response.text)
+
+
+
+
+
 
 
