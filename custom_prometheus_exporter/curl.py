3140f9de7c5f35114ce54417c5b05082dc8ab643
Mikhailov Maksim <m.m.o.99@mail.ru>
2023-03-22 22:31:12 +0300

    test exporter

--- custom_prometheus_exporter/curl.py
+++
@@ -1,5 +0,0 @@
-import requests
-#зависимость которая забирает значение курлом и отображает его в нужном виде
-
-response = requests.get('http://localhost:8001/mock/free_ip.txt')
-print(response.text)
--- custom_prometheus_exporter/main.py
+++ custom_prometheus_exporter/main.py
@@ -1,24 +1,21 @@
-from prometheus_client import Gauge, Info, Summary, Histogram, Counter, start_http_server
 import requests
-import time
+from prometheus_client import Metric, REGISTRY, CollectorRegistry, push_to_gateway
+from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily
 
-response = requests.get('http://localhost:8001/mock/free_ip.txt')
+class MyExporter(object):
+    def collect(self):
+        response = requests.get('https://api.example.com')
+        json_data = response.json()
 
-ip = Info('free_ip:', 'use_ip:')
-ip.info({response.text: ''})
+        metric = GaugeMetricFamily('example_metric', 'Example metric description', labels=['label1'])
+        metric.add_metric(['value1'], json_data['value'])
 
-def ip(t):
-#    """A dummy function that takes some time."""
-    time.sleep(t)
+        yield metric
 
 if __name__ == '__main__':
-    # Start up the server to expose the metrics.
-    start_http_server(8000)
-    # Generate some requests.
-    while True:
-        print(response.text)
-
-
+    registry = CollectorRegistry()
+    registry.register(MyExporter())
+    push_to_gateway('localhost:9091', job='my_job', registry=registry)
 
 
 
