ab495cf70655827c4fe50aeb4b47d892809a31d7
Mikhailov Maksim <m.m.o.99@mail.ru>
2023-03-23 15:00:39 +0300

    надо тестить

--- custom_prometheus_exporter/3.py
+++ custom_prometheus_exporter/3.py
@@ -10,3 +10,35 @@ if response.status_code == 200:  # если запрос выполнен усп
 
     # вывод метрик в формате Prometheus
     print('metric1 {}\nmetric2 {}'.format(metric1, metric2))
+
+    import http.server
+    import urllib.parse
+    import urllib.request
+
+    PORT = 8000
+
+
+    class MyHandler(http.server.BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            self.wfile.write(b'<html><body><h1>GET Request Received!</h1></body></html>')
+
+        def do_POST(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            data = urllib.parse.urlencode({'key': 'value'}).encode('ascii')
+            response = urllib.request.urlopen(url="your_api_url", data=data).read()
+            response_data = response.decode('ascii')
+
+            self.wfile.write(bytes(response_data, "utf-8"))
+
+
+    httpd = http.server.HTTPServer(('localhost', PORT), MyHandler)
+
+    print("serving at port", PORT)
+    httpd.serve_forever()
