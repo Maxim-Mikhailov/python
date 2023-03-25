fdc82f140f5ec7a218c05a0dc011400035bc4467
Mikhailov Maksim <m.m.o.99@mail.ru>
2023-03-25 16:23:00 +0300

    fix cycle for request

--- custom_prometheus_exporter/api_request_prometheus_exporter.py
+++ custom_prometheus_exporter/api_request_prometheus_exporter.py
@@ -7,6 +7,7 @@ start_http_server(8000)
 # создаем метрики для сбора статистки
 c = Counter('api_request_total', 'Total number of requests to API server')
 g = Gauge('api_free_ips', 'Number of free IPs returned from API server')
+g2 = Gauge('api_total_ips', "Total number IPs returned from API server")
 
 # URL API сервера
 url = 'https://example/microservice/api/v1/cluster_stats/'
@@ -26,11 +27,14 @@ if response.status_code == 200:
     # если запрос прошел успешно, парсим ответ и собираем значение
     result = response.json()
     free_ips = result['data']['edz']['data_free_ips']
+    total_ips = result['data']['edz']['data_total_ips']
     if free_ips is not None:
         c.inc()
         g.set(free_ips)
+        g2.set(total_ips)
 
 # выводим метрики в формате Prometheus на страницу http.server
 while True:
     print("api_request_total: ", c._value.get())
     print("api_free_ips: ", g._value.get())
+    print("api_total_ips", g2._value.get())
