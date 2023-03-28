7f4695d8a71b146ae47cce577705a80483aa424d
Mikhailov Maksim <m.m.o.99@mail.ru>
2023-03-28 22:24:47 +0300

    new counter

--- custom_prometheus_exporter/api_request_prometheus_exporter.py
+++ custom_prometheus_exporter/api_request_prometheus_exporter.py
@@ -1,40 +1,46 @@
-import requests, json
+import requests
+import json
+from time import sleep
 from prometheus_client import Counter, Gauge, start_http_server
 
-# стартуем сервер Prometheus
+# Стартуем сервер Prometheus
 start_http_server(8000)
 
-# создаем метрики для сбора статистки
+# Создаем метрики для сбора статистики
 c = Counter('api_request_total', 'Total number of requests to API server')
-g = Gauge('api_free_ips', 'Number of free IPs returned from API server')
-g2 = Gauge('api_total_ips', "Total number IPs returned from API server")
+g_free_ips = Gauge('api_free_ips', 'Number of free IPs returned from API server')
+g_total_ips = Gauge('api_total_ips', "Total number IPs returned from API server")
+c_req_failed = Counter('api_request_failed_total', 'Total number of failed requests to API server')
 
 # URL API сервера
 url = 'https://example/microservice/api/v1/cluster_stats/'
-# запрос к api
-data = {
-  "msg_id": "7772fc59-453f-****-****-************",
-  "correlation_id": "7772fc59-453f-****-****-************",
-  "customer": "mon_di",
-  "serial_number": "REGION-**********01"
-}
 
-# отправляем POST-запрос с параметром "data"
-response = requests.post(url, data=json.dumps(data), verify=False, timeout=(120, 120))
-
-# проверяем код ответа
-if response.status_code == 200:
-    # если запрос прошел успешно, парсим ответ и собираем значение
-    result = response.json()
-    free_ips = result['data']['edz']['data_free_ips']
-    total_ips = result['data']['edz']['data_total_ips']
-    if free_ips is not None:
-        c.inc()
-        g.set(free_ips)
-        g2.set(total_ips)
-
-# выводим метрики в формате Prometheus на страницу http.server
 while True:
+    # Запрос к API
+    data = {
+      "msg_id": "7772fc59-453f-****-****-************",
+      "correlation_id": "7772fc59-453f-****-****-************",
+      "customer": "mon_di",
+      "serial_number": "REGION-**********01"
+    }
+    response = requests.post(url, data=json.dumps(data), verify=False, timeout=(120, 120))
+    # Проверяем код ответа
+    if response.status_code == 200:
+        # Если запрос прошел успешно, парсим ответ и собираем значение
+        result = response.json()
+        free_ips = result['data']['edz']['data_free_ips']
+        total_ips = result['data']['edz']['data_total_ips']
+        if free_ips is not None:
+            c.inc()
+            g_free_ips.set(free_ips)
+            g_total_ips.set(total_ips)
+    else:
+        # Если запрос не прошел успешно, увеличиваем счетчик неудачных запросов
+        c_req_failed.inc()
+
+    # Выводим метрики в формате Prometheus на страницу http.server
     print("api_request_total: ", c._value.get())
-    print("api_free_ips: ", g._value.get())
-    print("api_total_ips", g2._value.get())
+    print("api_request_failed_total: ", c_req_failed._value.get())
+    print("api_free_ips: ", g_free_ips._value.get())
+    print("api_total_ips", g_total_ips._value.get())
+    sleep(30)
